01bbb58982cd0cf5af3b50f3b87321fed52df058
nkororo <150944984+nkororo@users.noreply.github.com>
2026-01-31 11:20:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,9 @@ function App() {
           <p>
             Proiecte dezvoltate cu pasiune.
           </p>
+          <p>
+            Junior Software Developer în formare, cu un interes deosebit pentru backend și baze de date. Dețin cunoștințe teoretice și practice în Java, Python și JS, aplicate în proiecte de tip personal/cursuri. Sunt în căutarea primului rol profesional unde să pot pune în practică bazele acumulate și să evoluez alături de o echipă experimentată.
+          </p>
         </div>
       </header>
 
